6f8d2e6941c773e907213802acbe7425f6b1ae5f
Filipe Goncalves <filipe.carrao_goncalves.ext@siemens.com>
2024-09-21 16:28:04 +0100

    feat: adding simple api to gochain

--- src/main.go
+++ src/main.go
@@ -2,12 +2,52 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	gochain "github.com/filipemcg/gochain/pkg"
+	"github.com/gin-gonic/gin"
 )
 
+var blockChain = []gochain.Block{}
+
+func getBlock(c *gin.Context) {
+	hash := c.Param("hash")
+	for _, block := range blockChain {
+		if fmt.Sprintf("%x", block.Hash) == hash {
+			serializedBlock := map[string]interface{}{
+				"Number": block.Number,
+				"Data":   block.Data,
+				"Nonce":  block.Nonce,
+				"Prev":   fmt.Sprintf("%x", block.Prev),
+				"Hash":   fmt.Sprintf("%x", block.Hash),
+			}
+			c.JSON(http.StatusOK, serializedBlock)
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"message": "block not found"})
+}
+
+func postBlock(c *gin.Context) {
+	data := c.PostForm("data")
+	prev := blockChain[len(blockChain)-1].Hash
+	newBlock := gochain.NewBlock(len(blockChain)+1, data, prev)
+	blockChain = append(blockChain, *newBlock)
+
+	serializedBlock := map[string]interface{}{
+		"Number": newBlock.Number,
+		"Data":   newBlock.Data,
+		"Nonce":  newBlock.Nonce,
+		"Prev":   fmt.Sprintf("%x", newBlock.Prev),
+		"Hash":   fmt.Sprintf("%x", newBlock.Hash),
+	}
+	c.JSON(http.StatusCreated, serializedBlock)
+}
+
 func main() {
-	blockChain := []gochain.Block{}
+	r := gin.Default()
+
+	blockChain = []gochain.Block{}
 
 	genisesPrevBytes := [32]byte{0}
 
@@ -15,7 +55,9 @@ func main() {
 
 	blockChain = append(blockChain, *genisesBlock)
 
-	for _, b := range blockChain {
-		fmt.Printf("Block{Block#: %d, Data: %s, Nonce: %d, Prev: %x, Hash: %x}\n", b.Number, b.Data, b.Nonce, b.Prev, b.Hash)
-	}
+	r.GET("/blocks/:hash", getBlock)
+	r.POST("/blocks", postBlock)
+
+	fmt.Println("Starting server on :8080")
+	r.Run(":8080")
 }
